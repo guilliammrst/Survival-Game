5be793171130ea00dde922f26ea0d54a1e285191
guilliammrst <guilliam.morisset85@gmail.com>
2024-02-29 10:22:53 +0100

    Add items actions (drop and destroy)

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -16,6 +16,38 @@ public class Inventory : MonoBehaviour
 
     const int InventorySize = 24;
 
+    [Header("Action Panel References")]
+
+    [SerializeField]
+    private GameObject actionPanel;
+
+    [SerializeField]
+    private GameObject useItemButton;
+
+    [SerializeField]
+    private GameObject equipItemButton;
+
+    [SerializeField]
+    private GameObject dropItemButton;
+
+    [SerializeField]
+    private GameObject destroyItemButton;
+
+    private ItemData itemCurrentlySelected;
+    
+    [SerializeField]
+    private Sprite emptySlotVisual;
+
+    [SerializeField]
+    private Transform dropPoint;
+
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     private void Start()
     {
         RefreshContent();
@@ -26,6 +58,11 @@ public class Inventory : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.I))
         {
             inventoryPanel.SetActive(!inventoryPanel.activeSelf);
+
+            if (actionPanel.activeSelf)
+            {
+                actionPanel.SetActive(false);
+            }
         }
     }
 
@@ -38,10 +75,20 @@ public class Inventory : MonoBehaviour
     public void CloseInventory()
     {
         inventoryPanel.SetActive(false);
+        actionPanel.SetActive(false);
     }
 
     private void RefreshContent()
     {
+        // Vider visuel inventaire
+        for(int i = 0; i < inventorySlotsParent.childCount; i++)
+        {
+            Slot currentSlot = inventorySlotsParent.GetChild(i).GetComponent<Slot>();
+            currentSlot.item = null;
+            currentSlot.itemVisual.sprite = emptySlotVisual;
+        }
+
+        // Repeupler l'inventaire
         for (int i = 0; i < items.Count; i++)
         {
             Slot currentSlot = inventorySlotsParent.GetChild(i).GetComponent<Slot>();
@@ -53,5 +100,69 @@ public class Inventory : MonoBehaviour
     public bool IsFull()
     {
         return InventorySize == items.Count;
+    }
+
+    public void OpenActionPanel(ItemData item, Vector3 slotPosition)
+    {
+        itemCurrentlySelected = item;
+
+        if (item == null)
+        {
+            actionPanel.SetActive(false);
+            return;
+        }
+
+        switch (item.itemType)
+        {
+            case ItemType.Resource:
+                useItemButton.SetActive(false);
+                equipItemButton.SetActive(false);
+                break;
+            case ItemType.Equipment:
+                useItemButton.SetActive(false);
+                equipItemButton.SetActive(true);
+                break;
+            case ItemType.Consumable:
+                useItemButton.SetActive(true);
+                equipItemButton.SetActive(false);
+                break;
+        }
+
+        actionPanel.transform.position = slotPosition;
+        actionPanel.SetActive(true);
+    }
+
+    public void CloseActionPanel()
+    {
+        actionPanel.SetActive(false);
+        itemCurrentlySelected = null;
+    }
+
+    public void UseActionButton() 
+    {
+        print("Using item : " + itemCurrentlySelected.name);
+        CloseActionPanel();
+    }
+
+    public void EquipActionButton() 
+    {
+        print("Equiping item : " + itemCurrentlySelected.name);
+        CloseActionPanel();
+    }
+
+    public void DropActionButton() 
+    {
+        GameObject instantiatedItem = Instantiate(itemCurrentlySelected.prefab);
+        instantiatedItem.transform.position = dropPoint.position;
+        items.Remove(itemCurrentlySelected);
+        RefreshContent();
+        CloseActionPanel();
+    }
+
+    public void DestroyActionButton() 
+    {
+        items.Remove(itemCurrentlySelected);
+        RefreshContent();
+        CloseActionPanel();
     }
 }
